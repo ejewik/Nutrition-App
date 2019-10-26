dc1e76afa758bcb4029ecae38b87fac47adc1e49
Emily Jewik <emilyjewik@gmail.com>
2019-10-26 15:52:22 -0700

    Commit will check camera and write to permissions

--- app/src/main/java/com/example/mynutrition/MainActivity.java
+++ app/src/main/java/com/example/mynutrition/MainActivity.java
@@ -2,13 +2,52 @@ package com.example.mynutrition;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int PERMISSION_CODE = 1000;
+
+    //Had to auto-complete for android widget?
+    Button mCaptureBtn;
+    ImageView mImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mImageView = findViewById(R.id.image_view);
+        mCaptureBtn = findViewById(R.id.capture_image_btn);
+
+        //on button click
+        mCaptureBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //if system os is >= marshmallow, request runtime permissions
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    if (checkSelfPermission(Manifest.permission.CAMERA) ==
+                            PackageManager.PERMISSION_DENIED || checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
+                        //permissions not enabled, request it
+                        String[] permission = {Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+                        //show popup to request permissions
+                        requestPermissions(permission, PERMISSION_CODE);
+                    }
+                    else {
+                        //permissions granted already
+                    }
+                }
+                else {
+                    //system os < marshmallow
+                }
+            }
+        });
     }
 }
