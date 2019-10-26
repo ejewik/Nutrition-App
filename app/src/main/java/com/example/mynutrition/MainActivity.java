f7481c2a1ba7e3273bca2408f16d0eac974e4c80
Emily Jewik <emilyjewik@gmail.com>
2019-10-26 16:48:37 -0700

    Finished the camera function and storing image uri

--- app/src/main/java/com/example/mynutrition/MainActivity.java
+++ app/src/main/java/com/example/mynutrition/MainActivity.java
@@ -1,22 +1,32 @@
 package com.example.mynutrition;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
+import android.content.ContentValues;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int PERMISSION_CODE = 1000;
+    private static final int IMAGE_CAPTURE_CODE = 1001;
 
     //Had to auto-complete for android widget?
     Button mCaptureBtn;
     ImageView mImageView;
+
+    Uri image_uri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,12 +52,55 @@ public class MainActivity extends AppCompatActivity {
                     }
                     else {
                         //permissions granted already
+                        openCamera();
                     }
                 }
                 else {
                     //system os < marshmallow
+                    openCamera();
                 }
             }
         });
     }
+
+    private void openCamera() {
+        ContentValues values = new ContentValues();
+        values.put(MediaStore.Images.Media.TITLE, "New Picture");
+        values.put(MediaStore.Images.Media.DESCRIPTION, "From the Camera");
+        image_uri = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+        //Camera intent
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, image_uri);
+        startActivityForResult(cameraIntent, IMAGE_CAPTURE_CODE);
+    }
+
+    //handling permission result
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults ) {
+        //this method is called, when user presses Allow or Deny from Permission Request Popup
+        switch(requestCode) {
+            case PERMISSION_CODE: {
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    //permission granted from popup
+                    openCamera();
+                }
+                else {
+                    Toast.makeText(this, "Permission denied...", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        //called when image is captured from camera
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode == RESULT_OK) {
+            //set the image captured to our ImageView
+            mImageView.setImageURI(image_uri);
+            Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), image_uri);
+        }
+    }
+
 }
